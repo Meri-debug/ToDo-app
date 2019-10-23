689af84443511be814d44930ff29a12cdf28c652
Kagenuge <kaarlen@gmail.com>
2019-10-23 10:42:21 +0300

    finished POST & PUT requests, and tests

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,75 @@
 var express = require('express');
 var router = express.Router();
+const uuid = require('uuid');
+const fs = require('fs');
 
-const list = [
+var list = [
   {
     id: 1,
-    title: 'Splägä',
-    radio: 'done'
+    title: "Joo",
+    deadline: "21.11.2019",
+    completed: true,
+    priority: "high"
   },
   {
     id: 2,
-    title: 'Test data number two',
-    radio: 'done'
+    title: "Kaarlen Synttärit",
+    deadline: "11.09.1995",
+    completed: true,
+    priority: "ultra high"
   },
   {
     id: 3,
-    title: 'Go to the market',
-    radio: 'done'
-  },
-  {
-    id: 4,
-    title: 'Buy some green bois',
-    radio: 'done'
+    title: "Plebeiji",
+    deadline: "1.11.2014",
+    completed: false,
+    priority: "low"
   },
 ]
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/api/list', function(req, res, next) {
+  const newitem = {
+    id: uuid(),
+    title: req.body.title,
+    deadline: req.body.time,
+    completed: req.body.completed,
+    priority: req.body.priority
+  };
+
+  list.push(newitem);
+  console.log(list);
+
+  const jsonList = JSON.stringify(list, null, 2);
+
+  fs.writeFileSync(__dirname+'/../public/list.json', jsonList, function(err) {
+    if (err) throw err;
+  });
+
+  res.status(201);
+  res.json(list);
+})
+
+router.put('/api/list/:id', function(req, res, next) {
+  const id = req.params.id;
+  let item = list.filter(item => {
+    return item.id == id;
+  })[0];
+  const index = list.indexOf(item);
+  const keys = Object.keys(req.body);
+
+  keys.forEach(key => {
+    item[key] = req.body[key];
+  });
+
+  list[index] = item;
+
+  res.status(200);
+  res.json(list[index]);
 });
 
 module.exports = router;
--- tests/backend.test.js
+++ tests/backend.test.js
@@ -1,5 +1,25 @@
 const request = require('supertest');
+const app = require('../app');
 
-test('/api/list POST request toimii', (done) => {
-  return request (app)
-})
+test('/api/list POST request works', (done) => {
+  return request(app)
+    .post('/api/list').send({ title: "POST TEST", deadline: "done", completed: true, priority: "high" })
+    .then(res => {
+      expect(res.statusCode).toBe(201);
+      expect(res.body).toBeDefined();
+      expect(res.body.length).toBeGreaterThanOrEqual(1);
+      done();
+    });
+});
+
+test('/api/list PUT request works', (done) => {
+  const testdata = { title:'PUT TEST', deadline: '01.01.1600', completed: true, priority: 'test' }
+  return request(app)
+    .put('/api/list/1').send(testdata).then(res => {
+      expect(res.statusCode).toBe(200);
+      expect(res.body).toBeDefined();
+      console.log(res.body)
+      expect(res.body).toBeDe(testdata);
+      done();
+    });
+});
