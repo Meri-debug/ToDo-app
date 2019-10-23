278a68b3ce7a2e0e956b1b30371135be28881917
Kagenuge <kaarlen@gmail.com>
2019-10-23 16:31:27 +0300

    coded delTodo

--- public/script.js
+++ public/script.js
@@ -21,7 +21,7 @@ function getTodos() {
                 // const deadLine = todo.time; // need correct API values
                 const status = todo.completed; // need correct API values
                 const todoId = todo.id;
-                
+
                 var node = document.createElement("LI");
                 node.classList.add("list-group-item");
 
@@ -44,10 +44,6 @@ function getTodos() {
                 </div>
             `
                 toDoUl.appendChild(node);
-
-                document.getElementById('mod' + todoId).addEventListener('click', putTodo);
-
-                document.getElementById('del' + todoId).addEventListener('click', delTodo);
 
                 idCounter += 1;
             });
@@ -78,7 +74,7 @@ function postTodo() {
     })
         .then((data) => console.log(data))
         .catch((err) => console.log(err));
-    
+
     dateInput.value = "";
     title.value = "";
     radio = false;
@@ -93,10 +89,23 @@ function putTodo(clicked_id) {
     const id = clicked_id.toString().substring(3);
     console.log(id)
 
+    let dateInput = document.getElementById("dateinput");
+
+    let title = titleInput.value;
+    let deadLine = dateInput.value;
+    let radio = document.querySelector('input[name="radiobtn"]:checked').value;
+
+    let newToDo = {
+        title: title,
+        deadline: deadLine,
+        completed: false,
+        priority: radio
+    };
+
     fetch('http://localhost:3000/api/list/', modTodo)
         .then((res) => res.json())
         .then(data => data.map(post => {
-            
+
         }))
         .catch((err) => console.log(err))
 }
@@ -104,9 +113,21 @@ function putTodo(clicked_id) {
 // GET all todos on page load
 getTodos();
 
-function delTodo(clicked_id) {
-    console.log(clicked_id);
-}
+function delTodo(event) {
+    const id = event.substring(3);
+    console.log(id);
+
+    fetch(`http://localhost:3000/api/list/${id}`, {
+        method: 'DELETE',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify({ id })
+    })
+        .then(res => res.json())
+        .catch((err) => console.log(err))
+
+        $('#todoul').empty();
+        getTodos();
+};
 
 
 
