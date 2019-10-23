3339261ebd959192584c74f414f7f0c57beea34a
Max <maxkorpinen@gmail.com>
2019-10-23 16:23:17 +0300

    updates for the POST request

--- public/script.js
+++ public/script.js
@@ -3,6 +3,7 @@ const toDoUl = document.getElementById("todoul");
 let titleInput = document.getElementById("titleinput");
 let statusInput = document.getElementById("status");
 let addButton = document.getElementById("addbutton");
+let radioInput = document.querySelector('input[name="radiobtn"]')
 // let radioInput = document.querySelector('input[name="radiobtn"]:checked').value;
 
 let id = [];
@@ -26,13 +27,12 @@ function getTodos() {
                 const status = todo.completed; // need correct API values
                 const todoId = todo.id;
                 id.push(todoId);
-                console.log(id);
                 
                 var node = document.createElement("LI");
                 node.classList.add("list-group-item");
 
                 node.innerHTML =
-                    `
+                `
                 <div class="custom-control custom-checkbox">
                     <input type="checkbox" class="custom-control-input" id="check${idCounter}">
                     <label class="custom-control-label" for="check${idCounter}">${title}</label>
@@ -48,7 +48,7 @@ function getTodos() {
                         </span>
                     </div>
                 </div>
-            `
+                `
                 toDoUl.appendChild(node);
 
                 document.getElementById('mod' + idCounter).addEventListener('click', putTodo);
@@ -63,6 +63,8 @@ function getTodos() {
 // POST a new todo to server
 function postTodo() {
     let dateInput = document.getElementById("dateinput");
+
+    // let radioInput = document.querySelector('input[name="radiobtn"]:checked')
 
     let title = titleInput.value;
     let deadLine = dateInput.value;
@@ -82,12 +84,13 @@ function postTodo() {
     }).then(function (res) {
         res.json()
     })
-        .then((data) => console.log(data))
+        //.then((data) => console.log(data))
         .catch((err) => console.log(err));
     
     dateInput.value = "";
-    title.value = "";
-    radio = false;
+    titleInput.value = "";
+    radioInput.value = false;
+    getTodos();
 }
 
 // DELETE a todo
