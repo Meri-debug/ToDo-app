e1f6abc65a87326fbbf8d60ffa64b0b3c792e687
Max <maxkorpinen@gmail.com>
2019-10-23 11:07:06 +0300

    modified the JS file to match HTML

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const toDoArea = document.getElementById("testdiv"); // need correct ID name from HTML file
+const toDoUl = document.getElementById("todoul");
 
 let titleInput = document.getElementById("titleinput");
 let dateInput = document.getElementById("dateinput");
@@ -14,7 +14,8 @@ function getTodos() {
     fetch('https://jsonplaceholder.typicode.com/todos') // demodata
     .then(response => response.json())
     .then(function(json) {
-        let todoItem = '';
+
+        let idCounter = 1;
 
         json.forEach((todo) => {
             const title = todo.title; // need correct API values
@@ -22,6 +23,26 @@ function getTodos() {
             const status = todo.completed; // need correct API values
             const todoId = todo.id;
 
+            var node = document.createElement("LI");
+            node.classList.add("list-group-item");
+            node.innerHTML = 
+            `
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="check${idCounter}">
+                    <label class="custom-control-label" for="check${idCounter}">${title}</label>
+                </div>
+            `
+
+            toDoUl.appendChild(node);
+
+            idCounter += 1;
+
+            /*
+
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="check1" checked>
+                <label class="custom-control-label" for="check1">Example todo item</label>
+            </div>
             todoItem +=
                 `<div class="toDoCard" id="${todoId}">
                     <p><strong>Task:</strong> ${title}</p> ` +
@@ -29,7 +50,20 @@ function getTodos() {
                     `<p><strong>In progress?</strong> ${status}</p>
                 </div>`
 
+                var node = document.createElement("LI");                 // Create a <li> node
+                var textnode = document.createTextNode("Water");         // Create a text node
+                node.appendChild(textnode);                              // Append the text to <li>
+                document.getElementById("myList").appendChild(node);     // Append <li> to <ul> with id="myList"
+
+                <li class="list-group-item">
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="check1" checked>
+                        <label class="custom-control-label" for="check1">To do item 01</label>
+                    </div>
+                </li>
+
             toDoArea.innerHTML = todoItem;
+            */
         });
     });
 }
